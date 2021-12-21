33c9eb34b97a9a40d4e7b14bdf01dc847861b18c
michelle-salt <69985466+michelle-salt@users.noreply.github.com>
2021-12-21 14:47:33 -0500

    Update solution for Two Sum

--- Solutions/Array/twoSum.py
+++ Solutions/Array/twoSum.py
@@ -1,9 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         numWanted = 0
-        length = len(nums)
-        for i in range(length):
+        for i in range(len(nums)):
             numWanted = target - nums[i]
-            for j in range(i+1, length):
+            for j in range(i+1, len(nums)):
                 if nums[j]==numWanted:
                     return [i, j]
