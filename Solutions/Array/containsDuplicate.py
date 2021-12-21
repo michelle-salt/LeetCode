4921068abb74062e0dab49c1b8d27079509cf6fc
michelle-salt <69985466+michelle-salt@users.noreply.github.com>
2021-12-21 15:13:08 -0500

    New solution for Contains Duplicate

--- Solutions/Array/containsDuplicate.py
+++ Solutions/Array/containsDuplicate.py
@@ -1,4 +1,13 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(set(nums)) != len(nums)
-        
+        # //for each element in array
+        #     //if num is in dictionary, return false
+        #     //otherwise, add it to dictionary
+            
+        all_nums = {}
+        for num in nums:
+            if (all_nums.get(num) != None):
+                return True;
+            else:
+                all_nums.update({num: 1});
+        return False;
