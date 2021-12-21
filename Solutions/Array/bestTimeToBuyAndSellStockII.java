8321c0cc6e53f34a9e29d6d27d6c65926f6df3fd
michelle-salt <69985466+michelle-salt@users.noreply.github.com>
2021-12-21 15:09:38 -0500

    Update solution for Best Time to Buy and Sell Stock II

--- Solutions/Array/bestTimeToBuyAndSellStockII.java
+++ Solutions/Array/bestTimeToBuyAndSellStockII.java
@@ -1,19 +1,9 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int hold = -1;
         int profit = 0;
-        for (int i = 0; i < prices.length; i++) {
-            if (hold != -1) { //If you're holding
-                if (i == prices.length -1) {
-                    profit += prices[i] - prices[hold];
-                } else if (prices[i+1] < prices[i]) {
-                    profit += prices[i] - prices[hold];
-                    hold = -1;
-                }
-            } else { //if not holding
-                if (i != prices.length -1 && prices[i+1] > prices[i]) {
-                    hold = i;
-                }
+        for (int i = 0; i < prices.length -1; i++) {
+            if (prices[i] < prices[i+1]) {
+                profit += prices[i+1] - prices[i];
             }
         }
         return profit;
