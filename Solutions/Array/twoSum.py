aee7c234cbdcd699f0d7659f21d13f194dcfd95f
michelle-salt <69985466+michelle-salt@users.noreply.github.com>
2021-12-21 15:16:52 -0500

    New solution for Two Sum

--- Solutions/Array/twoSum.py
+++ Solutions/Array/twoSum.py
@@ -1,8 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        numWanted = 0
-        for i in range(len(nums)):
-            numWanted = target - nums[i]
-            for j in range(i+1, len(nums)):
-                if nums[j]==numWanted:
-                    return [i, j]
+        #7 - [2, 7, 3, 8, 19, 4]
+        
+        #for num in nums
+            #if target - num is in dictionary (key)
+            #return dictionary value (index) + current index
+            #otherwise, add to dictionary (key --> num, value --> index)
+            
+        previousNums = {}
+        indexCounter = 0
+        for num in nums:
+            if previousNums.get(target - num) != None:
+                return [previousNums.get(target - num), indexCounter]
+            previousNums.update({num : indexCounter})
+            indexCounter += 1
