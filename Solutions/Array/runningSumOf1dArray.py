2faef64f3e03825791697893eebcf225a66caf78
michelle-salt <69985466+michelle-salt@users.noreply.github.com>
2021-12-21 14:50:24 -0500

    Update solution for Running Sum of 1d Array

--- Solutions/Array/runningSumOf1dArray.py
+++ Solutions/Array/runningSumOf1dArray.py
@@ -1,11 +1,5 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        runningSums = []
-        i = 0
-        for num in nums:
-            if i == 0:
-                runningSums.append(nums[i])
-            else: 
-                runningSums.append(nums[i] + runningSums[i-1])
-            i += 1
-        return runningSums
+        for i in range(1, len(nums)): 
+            nums[i] += nums[i-1]
+        return nums
